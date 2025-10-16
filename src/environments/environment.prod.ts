349aa1a80a707421223947f3f075a1751db353a3
jesusmariade <maria.garcia9579@alumnos.udg.mx>
2025-10-16 13:56:32 -0600

    Proyectofaceapiagregado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'detecfaceapi',
+    appId: '1:899627282373:web:4649b96cad131dbb6425ac',
+    storageBucket: 'detecfaceapi.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCsx9O-av6jMNCXXW3lLC0Z3pTpwhS2Ra4',
+    authDomain: 'detecfaceapi.firebaseapp.com',
+    messagingSenderId: '899627282373',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'detecfaceapi',
+    appId: '1:899627282373:web:4649b96cad131dbb6425ac',
+    storageBucket: 'detecfaceapi.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCsx9O-av6jMNCXXW3lLC0Z3pTpwhS2Ra4',
+    authDomain: 'detecfaceapi.firebaseapp.com',
+    messagingSenderId: '899627282373',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
